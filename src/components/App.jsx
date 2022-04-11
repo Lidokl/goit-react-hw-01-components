210b59008ec514a196d2ec6590db2403b6c196f4
Lidija Levchenko <levchenkola@ukr.net>
2022-04-11 21:56:52 +0300

    1 завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import { Profile } from 'components/socialProfile/Profile';
 import user from 'components/socialProfile/user.json';
+import styled from 'components/socialProfile/Profile.module.css';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div className={styled.App}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
   );
 };
--- src/components/socialProfile/Profile.jsx
+++ src/components/socialProfile/Profile.jsx
@@ -1,31 +1,41 @@
-const Profile = ({ username, tag, location, avatar, stats }) => {
+import PropTypes from 'prop-types';
+import styled from 'components/socialProfile/Profile.module.css';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class="profile">
-      <div class="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+    <div className={styled.profile}>
+      <div className={styled.description}>
+        <img src={avatar} alt={username} className={styled.avatar} />
+        <p className={styled.name}>{username}</p>
+        <p className={styled.tag}>@{tag}</p>
+        <p className={styled.location}>{location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+      <ul className={styled.statsList}>
+        <li className={styled.statsListItem}>
+          <span className={styled.label}>Followers</span>
+          <span className={styled.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">2000</span>
+        <li className={styled.statsListItem}>
+          <span className={styled.label}>Views</span>
+          <span className={styled.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+        <li className={styled.statsListItem}>
+          <span className={styled.label}>Likes</span>
+          <span className={styled.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
+};
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    likes: PropTypes.number,
+    views: PropTypes.number,
+    followers: PropTypes.number,
+  }).isRequired,
 };
